dad8d39c4f62abf9f26d4b6ba0c6a6a4da9dd415
Denis Oliveira Ramos <denoso19937@gmail.com>
2026-05-07 12:15:14 -0300

    fix: ajustar contexto correto do cluster no provider

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "/home/denoso/.kube/config"
+  config_context = "kind-lab-sre-denoso"
+}
